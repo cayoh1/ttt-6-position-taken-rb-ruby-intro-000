4d28b57f4364db7a63b93ac0f5a03a7e4836601c
Christine Ayoh <christineayoh9@gmail.com>
2020-04-29 04:32:33 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,12 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, index)
-  if board[index] == ""
-    return false
+  if board[index] == "X"
+    return true
+
+  elsif board[index] == "O"
+    return true
+
   else
     return true
   end
